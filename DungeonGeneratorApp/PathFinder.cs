89ac4b13ae30ebe6b2e79445d63a1c36f5e2b4f2
Feycry <78263543+Feycry@users.noreply.github.com>
2025-10-01 23:02:04 +0300

    Began implementing pathfinding logic in PathFinder class

--- DungeonGeneratorApp/PathFinder.cs
+++ DungeonGeneratorApp/PathFinder.cs
@@ -11,8 +11,38 @@ public class PathFinder
         this.grid = grid;
     }
 
+    //A* pathfinding
     public void FindPath(Room start, Room end)
     {
-        
+        Tile startTile = new Tile(start.X, start.Y);
+        Tile endTile = new Tile(end.X, end.Y);
+
+        List<Tile> openSet = new List<Tile>();
+        HashSet<Tile> closedSet = new HashSet<Tile>();
+
+        openSet.Add(startTile);
+
+        while (openSet.Count > 0)
+        {
+            Tile currentTile = openSet[0];
+
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].FCost < currentTile.FCost || (openSet[i].FCost == currentTile.FCost && openSet[i].HCost < currentTile.HCost))
+                {
+                    currentTile = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentTile);
+            closedSet.Add(currentTile);
+
+            if (currentTile == endTile)
+            {
+                return;
+            }
+
+
+        }
     }
 }
